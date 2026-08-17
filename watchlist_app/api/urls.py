72f02f4d8a41e972c371bacb85a9c76e28931eff
VanHungNguyen <33939159+VanHungNguyenn@users.noreply.github.com>
2026-08-17 23:48:10 +0700

    class view

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from watchlist_app.api.views import movie_details, movie_list
+# from watchlist_app.api.views import movie_details, movie_list
+from watchlist_app.api.views import MovieListAV, MovieDetailAV
 
 urlpatterns = [
-    path("list/", movie_list, name="movie-list"),
-    path("<int:pk>/", movie_details, name="movie-details"),
+    path("list/", MovieListAV.as_view(), name="movie-list"),
+    path("<int:pk>/", MovieDetailAV.as_view(), name="movie-details"),
 ]
